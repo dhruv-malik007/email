da177e71fbf1361a22d2e1e04890cad0b2558f8f
Dhruv Malik <dhruv3777@gmail.com>
2024-09-23 10:03:06 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, request, render_template
+from flask_mail import Mail, Message
+import logging
+
+app = Flask(__name__)
+
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT=465,  
+    MAIL_USE_SSL=True,
+    MAIL_USE_TLS=False,  
+    MAIL_USERNAME='dhruv3777@gmail.com', 
+    MAIL_PASSWORD="qxfd qtqu mfkv ymzh", 
+    MAIL_DEFAULT_SENDER='dhruv3777@gmail.com'
+)
+
+
+mail = Mail(app)
+
+logging.basicConfig(level=logging.INFO)
+
+
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    user_email = request.form.get('email')
+    user_name = request.form.get('name')
+    user_mobile = request.form.get('mobile')
+    password = request.form.get("password")
+
+    try:
+        msg = Message("Welcome to ACN",sender="dhruv.symb@gmail.com", recipients=[user_email])
+        msg.html = render_template("index.html", user_name=user_name,user_mobile=user_mobile,user_email=user_email)
+        mail.send(msg)
+        logging.info(f"Email sent successfully to {user_email}")
+        return "Signup successful, email sent!"
+    except Exception as e:
+        logging.error(f"Failed to send email: {e}")
+        return "Signup successful, but failed to send email."
+
+if __name__ == '__main__':
+    app.run(debug=True)
